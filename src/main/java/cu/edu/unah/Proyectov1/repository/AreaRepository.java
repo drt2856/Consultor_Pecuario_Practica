cae12dca33beb7b5dd22e352bd54822644c54f23
CarlosRCarrillo <carlosrafaelcarrillo349@gmail.com>
2023-01-06 10:48:52 -0500

    Hexagonal solo de la tabla Animal

--- src/main/java/cu/edu/unah/Proyectov1/repository/AreaRepository.java
+++
@@ -1,9 +0,0 @@
-package cu.edu.unah.Proyectov1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cu.edu.unah.Proyectov1.model.Area;
-
-public interface AreaRepository extends JpaRepository<Area, Integer>{
-
-}
