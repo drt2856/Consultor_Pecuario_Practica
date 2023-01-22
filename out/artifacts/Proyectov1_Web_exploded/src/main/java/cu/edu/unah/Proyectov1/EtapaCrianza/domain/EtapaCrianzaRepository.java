2653a1fed4baae48fbe349eddc67720813a8f467
drt2856 <101726237+drt2856@users.noreply.github.com>
2023-01-21 23:23:09 -0500

    Delete out directory

--- out/artifacts/Proyectov1_Web_exploded/src/main/java/cu/edu/unah/Proyectov1/EtapaCrianza/domain/EtapaCrianzaRepository.java
+++
@@ -1,7 +0,0 @@
-package cu.edu.unah.Proyectov1.EtapaCrianza.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EtapaCrianzaRepository extends JpaRepository<EtapaCrianza, EtapaCrianzaPK>{
-
-}
--- out/artifacts/Proyectov1_Web_exploded/src/main/java/cu/edu/unah/Proyectov1/GrupoAnimal/domian/GrupoanimalRepository.java
+++
@@ -1,9 +0,0 @@
-package cu.edu.unah.Proyectov1.GrupoAnimal.domian;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cu.edu.unah.Proyectov1.GrupoAnimal.domian.Grupoanimal;
-
-public interface GrupoanimalRepository extends JpaRepository<Grupoanimal, Integer>{
-
-}
